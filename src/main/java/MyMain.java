8f71e7cf9bbe4a588750b53ea38a8005ff5c677a
marycottrill <21mcottrill@athenian.org>
2020-10-01 04:07:48 +0000

    Array Iteration Code

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -1,25 +1,56 @@
+import java.util.Arrays;
 public class MyMain {
 
     // Reverses an array
     public static int[] reverse(int[] arr) {
-        // REPLACE WITH YOUR CODE HERE
-        return null;
+        for (int i = 0; i < arr.length / 2; i++){
+            int x = arr[i];
+            arr[i] = arr[arr.length - i - 1];
+            arr[arr.length - i - 1] = x;
+        }
+        return arr;
     }
 
     // Finds the second largest number in an array
     public static int secondLargest(int[] arr) {
-        // REPLACE WITH YOUR CODE HERE
-        return -1;
+        int largest = -99999;
+        int secondlargest = arr[0];
+        for (int i = 0; i < arr.length; i++){
+            if (arr[i] > largest){
+                secondlargest = largest;
+                largest = arr[i];
+            }
+            else if (arr[i] > secondlargest){
+                secondlargest = arr[i];
+            }
+        }
+        return secondlargest;
     }
 
     // Checks to see if an array contains a geometric series
     public static boolean isGeometric(int[] arr) {
-        // REPLACE WITH YOUR CODE HERE
-        return false;
+        if (arr.length <= 2){
+            System.out.println("This array is geometric.");
+            return true; 
+        }
+        double geometric = (double)arr[0]/(double)arr[1];
+        System.out.println(geometric);
+        for (int i = 0; i < arr.length - 1; i++){
+            if (geometric != (double)arr[i]/(double)arr[i + 1]){
+                System.out.println("This array is not geometric");
+                return false;
+            }   
+        }
+        System.out.println("The array is geometric.");
+        return true;
     }
-
 
     public static void main(String[] args) {
-        // YOUR CODE HERE
-    }
+        int arr[]= {1, 3, 9, 27, 81};
+        reverse(arr);
+        secondLargest(arr);
+        isGeometric(arr);
+        } 
+
 }
+
